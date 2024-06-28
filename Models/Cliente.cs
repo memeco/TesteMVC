6de1ef929101c08f82862ef7c624f951f66cc676
Emerson Marques Pedro <emersonmp@gmail.com>
2024-06-28 20:49:08 -0300

    Arrumei

--- Models/Cliente.cs
+++ Models/Cliente.cs
@@ -1,10 +1,14 @@
-public class Cliente
+namespace ClienteMvc.Models
 {
-    public int ID_Cliente { get; set; }
-    public string Nome { get; set; }
-    public string Endereco { get; set; }
-    public string Telefone { get; set; }
-    public string RG { get; set; }
-    public string CPF { get; set; }
-    public string Email { get; set; }
+    public class Cliente
+    {
+
+        public int ID_Cliente { get; set; }
+        public string Nome { get; set; }
+        public string Endereco { get; set; }
+        public string Telefone { get; set; }
+        public string RG { get; set; }
+        public string CPF { get; set; }
+        public string Email { get; set; }
+    }
 }
