76002097abb7784db43cce23fc3060570c038a61
Emerson Marques Pedro <emersonmp@gmail.com>
2024-06-28 21:42:49 -0300

    Adicionei ClienteController

---
+++ Controllers/ClienteController.cs
@@ -0,0 +1,125 @@
+using Microsoft.AspNetCore.Mvc;
+using TesteMVC.Models;
+using TesteMVC.Data;
+using System.Linq;
+
+namespace TesteMVC.Controllers
+{
+    public class ClienteController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ClienteController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Cliente
+        public IActionResult Index()
+        {
+            return View(_context.Clientes.ToList());
+        }
+
+        // GET: Cliente/Details/5
+        public IActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var cliente = _context.Clientes
+                .FirstOrDefault(m => m.Id == id);
+            if (cliente == null)
+            {
+                return NotFound();
+            }
+
+            return View(cliente);
+        }
+
+        // GET: Cliente/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Cliente/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create([Bind("Id,Nome,Email,Telefone")] Cliente cliente)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(cliente);
+                _context.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(cliente);
+        }
+
+        // GET: Cliente/Edit/5
+        public IActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var cliente = _context.Clientes.Find(id);
+            if (cliente == null)
+            {
+                return NotFound();
+            }
+            return View(cliente);
+        }
+
+        // POST: Cliente/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int id, [Bind("Id,Nome,Email,Telefone")] Cliente cliente)
+        {
+            if (id != cliente.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                _context.Update(cliente);
+                _context.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(cliente);
+        }
+
+        // GET: Cliente/Delete/5
+        public IActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var cliente = _context.Clientes
+                .FirstOrDefault(m => m.Id == id);
+            if (cliente == null)
+            {
+                return NotFound();
+            }
+
+            return View(cliente);
+        }
+
+        // POST: Cliente/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var cliente = _context.Clientes.Find(id);
+            _context.Clientes.Remove(cliente);
+            _context.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
