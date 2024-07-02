ea92e4ea2ec8fe7107b5de2c60b332a57815d58a
Emerson Marques Pedro <emersonmp@gmail.com>
2024-07-02 20:39:14 -0300

    Atualizei

--- Models/Cliente.cs
+++ Models/Cliente.cs
@@ -1,14 +1,45 @@
 namespace TesteMVC.Models
-{
-    public class Cliente
-    {
 
-        public int ID_Cliente { get; set; }
-        public string Nome { get; set; }
-        public string Endereco { get; set; }
-        public string Telefone { get; set; }
-        public string RG { get; set; }
-        public string CPF { get; set; }
-        public string Email { get; set; }
+    public class Cliente
+{
+    public string Nome { get; set; }
+    public string Email { get; set; }
+    public string Telefone { get; set; }
+}
+
+public class ListaDeClientes
+{
+    private List<Cliente> clientes = new List<Cliente>();
+
+    public void AdicionarCliente(Cliente cliente)
+    {
+        clientes.Add(cliente);
+    }
+
+    public List<Cliente> ObterClientes()
+    {
+        return clientes;
+    }
+
+    public Cliente ObterCliente(int id)
+    {
+        return clientes[id];
+    }
+
+    public void AtualizarCliente(int id, Cliente clienteAtualizado)
+    {
+        if (id >= 0 && id < clientes.Count)
+        {
+            clientes[id] = clienteAtualizado;
+        }
+    }
+
+    public void RemoverCliente(int id)
+    {
+        if (id >= 0 && id < clientes.Count)
+        {
+            clientes.RemoveAt(id);
+        }
     }
 }
+
